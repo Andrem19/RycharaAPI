26fcde28c6c2ec5dea9010fec25abf8357c1cb40
Andrem19 <7255591@gmail.com>
2022-02-08 05:33:25 +0000

    Add fibonacci

---
+++ RycharaLuckyBot/Indicators/FibonacciCalcNet.cs
@@ -0,0 +1,22 @@
+﻿namespace RycharaAPI.RycharaLuckyBot.Indicators
+{
+    public class FibonacciCalcNet
+    {
+        public static void FibL(double low, double high)
+        {
+            Variables.line236 = high - ((high - low) * 0.236);
+            Variables.line382 = high - ((high - low) * 0.382);
+            Variables.line5 = high - ((high - low) * 0.5);
+            Variables.line618 = high - ((high - low) * 0.618);
+            Variables.line786 = high - ((high - low) * 0.786);
+        }
+        public static void FibS(double high, double low2)
+        {
+            Variables.line236S = low2 + ((high - low2) * 0.236);
+            Variables.line382S = low2 + ((high - low2) * 0.382);
+            Variables.line5S = low2 + ((high - low2) * 0.5);
+            Variables.line618S = low2 + ((high - low2) * 0.618);
+            Variables.line786S = low2 + ((high - low2) * 0.786);
+        }
+    }
+}
--- Variables.cs
+++ Variables.cs
@@ -21,5 +21,23 @@
         public static string intervalFibLetter { get; set; } = "m";
         public static double amount { get; set; } = 0.5;
         public static double baseAmount { get; set; } = 0.5;
+
+        //Fibonacci Mod
+        public static double line236 { get; set; }
+        public static double line382 { get; set; }
+        public static double line5 { get; set; }
+        public static double line618 { get; set; }
+        public static double line786 { get; set; }
+
+        public static double line236S { get; set; }
+        public static double line382S { get; set; }
+        public static double line5S { get; set; }
+        public static double line618S { get; set; }
+        public static double line786S { get; set; }
+
+        public static double point1 { get; set; } = 0;
+        public static double point2 { get; set; } = 0;
+        public static double point3 { get; set; } = 0;
+        public static double oldPoint3 { get; set; } = 0;
     }
 }
